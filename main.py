cd5196330f87ed10fcee35d0e886b93e898329f3
rc19002 <rc19002@melville-high.school.nz>
2023-06-06 10:38:27 +1200

    yes

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from tkinter import ttk
 
 root = Tk()
 root.title("Temperature convertor")
+# text for the input boxes
 celsius_label = ttk.Label(root, text="Celsius")
 celsius_label.grid(row=0, column=0)
 fahrenheit_label = ttk.Label(root, text="Fahrenheit")
@@ -11,6 +12,14 @@ fahrenheit_label.grid(row=0, column=1)
 
 celsius_input = DoubleVar()
 fahrenheit_input = DoubleVar()
+value = 0
+
+
+def celc_calculator():
+    global value
+    value -= 32 * .5556
+    print(value)
+
 
 # user input box
 celsius_entry = ttk.Entry(root, textvariable=celsius_input)
@@ -22,7 +31,7 @@ fahrenheit_entry.grid(row=1, column=1, padx=5, pady=5)
 celsius_button = ttk.Button(root, text="Convert to Fahrenheit")
 celsius_button.grid(row=2, column=0, padx=5, pady=5)
 
-fahrenheit_button = ttk.Button(root, text="Convert to Celsius")
+fahrenheit_button = ttk.Button(root, text="Convert to Celsius", command=celc_calculator)
 fahrenheit_button.grid(row=2, column=1, padx=5, pady=5, sticky="WE")
 
 
@@ -52,7 +61,20 @@ conversion_log_button = ttk.Button(root, text="History", command=log_gui)
 conversion_log_button.grid(row=2, column=2, padx=5, pady=5)
 
 
-#closing the program with one button
+def temp_checker(min_value):
+    error = "Please enter a number that is more\n then {}".format(min_value)
+    try:
+        response = float(input("Enter a number: "))
+
+        if response < min_value:
+            print(error)
+        else:
+            return response
+    except ValueError:
+        print(error)
+
+
+# closing the program with one button
 def closing():
     root.destroy()
 
